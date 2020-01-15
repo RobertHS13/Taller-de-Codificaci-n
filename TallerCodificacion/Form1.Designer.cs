26dc43df68e6d3d54f4a753c8ad64d5f2e0271f6
RobertHS13 <robertohs13000@gmail.com>
2020-01-14 23:31:13 -0600

    mensaje

---
+++ TallerCodificacion/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TallerCodificacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtTexto = new System.Windows.Forms.RichTextBox();
+            this.txtExaminar = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.botonExaminar = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtxtTexto
+            // 
+            this.rtxtTexto.Location = new System.Drawing.Point(12, 12);
+            this.rtxtTexto.Name = "rtxtTexto";
+            this.rtxtTexto.Size = new System.Drawing.Size(435, 321);
+            this.rtxtTexto.TabIndex = 0;
+            this.rtxtTexto.Text = "";
+            // 
+            // txtExaminar
+            // 
+            this.txtExaminar.Location = new System.Drawing.Point(480, 108);
+            this.txtExaminar.Name = "txtExaminar";
+            this.txtExaminar.Size = new System.Drawing.Size(288, 20);
+            this.txtExaminar.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(480, 169);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(596, 169);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(725, 169);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // botonExaminar
+            // 
+            this.botonExaminar.Location = new System.Drawing.Point(782, 108);
+            this.botonExaminar.Name = "botonExaminar";
+            this.botonExaminar.Size = new System.Drawing.Size(75, 23);
+            this.botonExaminar.TabIndex = 5;
+            this.botonExaminar.Text = "Examinar";
+            this.botonExaminar.UseVisualStyleBackColor = true;
+            this.botonExaminar.Click += new System.EventHandler(this.botonExaminar_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(15, 348);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(858, 267);
+            this.dgv.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 627);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.botonExaminar);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtExaminar);
+            this.Controls.Add(this.rtxtTexto);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtTexto;
+        private System.Windows.Forms.TextBox txtExaminar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button botonExaminar;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ TallerCodificacion/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TallerCodificacion
+{
+    public partial class Form1 : Form
+    {
+        DataTable simbolos = new DataTable();
+        int pos, l, ns, nb, bpc, tb;
+
+        private void botonExaminar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog()
+            {
+                FileName = "Selecciona un archivo de texto",
+                Filter = "Text files (*.txt) | *.txt",
+                Title = "Seleccione archivo"
+            };
+
+            if(fd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var ruta = fd.FileName;
+                    txtExaminar.Text = ruta;
+                    rtxtTexto.Text = File.ReadAllText(ruta);
+                }
+                catch(SecurityException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                procesarTexto();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            simbolos.Columns.Add("Ind", typeof(int));
+            simbolos.Columns.Add("Simbolo", typeof(string));
+            simbolos.Columns.Add("Frec", typeof(int));
+            simbolos.Columns.Add("Visitado", typeof(bool));
+            simbolos.Columns.Add("Izq", typeof(int));
+            simbolos.Columns.Add("Der", typeof(int));
+            simbolos.Columns.Add("Código", typeof(string));
+            simbolos.Columns.Add("Bits", typeof(int));
+            simbolos.Columns.Add("Bits por caracter", typeof(int));
+
+            dgv.DataSource = simbolos;
+            dgv.Columns["Visitado"].Visible = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void procesarTexto()
+        {
+            string s, s1, s2;
+            int j, f1, f2, p1, p2;
+
+            l = rtxtTexto.TextLength;
+            j = -1;
+
+            //Ingresa los primeros nodos
+            for (int i = 0; i < l; i++)
+            {
+                s = rtxtTexto.Text.Substring(i, 1);
+                pos = buscarSimbolo(s);
+                if (pos >= 0)
+                    simbolos.Rows[pos][2] = Int32.Parse(simbolos.Rows[pos][2].ToString());
+                else
+                    simbolos.Rows.Add(new Object[] { j += 1, s, 1, false, -1, -1, "" });
+            }
+            ns = j;
+
+            //Generar árbol
+
+            for (int i = 0; i < ns; i++)
+            {
+                p1 = buscarMenor();
+                f1 = Int32.Parse(simbolos.Rows[p1][2].ToString());
+                s1 = simbolos.Rows[p1][1].ToString();
+                simbolos.Rows[p1]["Visitado"] = true;
+
+                p2 = buscarMenor();
+                f2 = Int32.Parse(simbolos.Rows[p2][2].ToString());
+                s2 = simbolos.Rows[p2][1].ToString();
+                simbolos.Rows[p2]["Visitado"] = true;
+
+                simbolos.Rows.Add(new Object[] { j += 1, s1 + s2, f1 + f2, false, p1, p2, "" });
+            }
+
+            //Generar Código
+
+            for (int i = 1; i <= ns; i++)
+            {
+                asignarCero(Int32.Parse(simbolos.Rows[simbolos.Rows.Count - i]["Izq"].ToString()));
+                asignarUno(Int32.Parse(simbolos.Rows[simbolos.Rows.Count - i]["Der"].ToString()));
+            }
+
+            //Calcular Longitudes
+
+
+        }
+
+        private void asignarCero(int p)
+        {
+            if (Int32.Parse(simbolos.Rows[p]["Izq"].ToString()) == -1)
+                simbolos.Rows[p]["Código"] = simbolos.Rows[p]["Código"].ToString() + "0";
+            else
+            {
+                asignarCero(Int32.Parse(simbolos.Rows[p]["Izq"].ToString()));
+                asignarCero(Int32.Parse(simbolos.Rows[p]["Der"].ToString()));
+            }
+        }
+
+        private void asignarUno(int p)
+        {
+            if (Int32.Parse(simbolos.Rows[p]["Der"].ToString()) == -1)
+                simbolos.Rows[p]["Código"] = simbolos.Rows[p]["Código"].ToString() + "1";
+            else
+            {
+                asignarUno(Int32.Parse(simbolos.Rows[p]["Izq"].ToString()));
+                asignarUno(Int32.Parse(simbolos.Rows[p]["Der"].ToString()));
+            }
+        }
+
+        private int buscarMenor()
+        {
+            int m, f, pos = 0;
+            bool b;
+            m = l + 1;
+            for (int i = 0; i < simbolos.Rows.Count; i++)
+            {
+                f = Int32.Parse(simbolos.Rows[i][2].ToString());
+                b = Boolean.Parse(simbolos.Rows[i]["Visitado"].ToString());
+                if(f<m && !b)
+                {
+                    m = f;
+                    pos = i;
+                }
+            }
+            return pos;
+        }
+
+        private int buscarSimbolo(string s)
+        {
+            int p = -1;
+            if(simbolos.Rows.Count > 1)
+                for (int i = 0; i < simbolos.Rows.Count; i++)
+                    if (s == simbolos.Rows[i][1].ToString())
+                        return i;
+            return p;
+        }
+    }
+}
